0ea2d298c373fd5222bc2d09fa712b12b672a2ad
thomas petiot <thomas.petiot.01@gmail.com>
2024-02-23 08:40:50 +0100

    ex2 rails serve

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'lists/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "lists#home"
+  resources :lists, only: [:show, :index, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
